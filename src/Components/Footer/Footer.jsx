4d217a138652537861b96454fdb9c98fd0872c19
deji2612 <olatigbay@gmail.com>
2025-10-26 14:44:14 +0000

    footer fix

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -92,53 +92,67 @@ const Footer = () => {
                     </ul>
                 </section> */}
             <section className='flex flex-col items-center'>
-            <h2 class="mb-2.5 font-rebrand-light text-sm uppercase text-rebrand-accent-100-contrast">
-                About Us
-            </h2>
-            <ul class="flex flex-col items-center list-none gap-3 p-0 w-full">
-                <li><a href="/about" id="11" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">About Us</a></li>
-                <li><a href="/diversity" id="13" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Diversity</a></li>
-                <li><a href="/students" id="15" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Students &amp; Graduates</a></li>
-                <li><a href="/essential-workers-discount" id="16" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Essential Workers Discount</a></li>
-                <li><a href="https://www.debenhamsgroup.com/sustainability/company-policies/#modern-slavery-statements" id="19" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Modern Slavery Statement</a></li>
-            </ul>
+                    <div className='flex flex-col items-start'>
+                       
+                        <h2 class="mb-2.5 font-rebrand-light text-sm uppercase text-rebrand-accent-100-contrast">
+                            About Us
+                        </h2>
+
+                        <ul className="flex flex-col  list-none gap-3 p-0 w-full">
+                            <li><a href="/about" id="11" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">About Us</a></li>
+                            <li><a href="/diversity" id="13" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Diversity</a></li>
+                            <li><a href="/students" id="15" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Students &amp; Graduates</a></li>
+                            <li><a href="/essential-workers-discount" id="16" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Essential Workers Discount</a></li>
+                            <li><a href="https://www.debenhamsgroup.com/sustainability/company-policies/#modern-slavery-statements" id="19" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Modern Slavery Statement</a></li>
+                        </ul>
+                    </div>
+                
             </section>
 
             <section className='flex flex-col items-center'>
-            <h2 class="mb-2.5 font-rebrand-light text-sm uppercase text-rebrand-accent-100-contrast">
-                Information
-            </h2>
-            <ul class="flex flex-col items-center list-none gap-3 p-0 w-full">
-                <li><a href="/terms" id="21" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Terms &amp; Conditions</a></li>
-                <li><a href="/privacy-policy" id="22" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Privacy Policy</a></li>
-                <li><a href="/site-map" id="23" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Sitemap</a></li>
-                <li><a href="/plt-app-info" id="311" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">App Info</a></li>
-            </ul>
+                <div className='flex flex-col items-start'>
+                    <h2 class="mb-2.5 font-rebrand-light text-sm uppercase text-rebrand-accent-100-contrast">
+                        Information
+                    </h2>
+                    <ul class="flex flex-col list-none gap-3 p-0 w-full">
+                        <li><a href="/terms" id="21" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Terms &amp; Conditions</a></li>
+                        <li><a href="/privacy-policy" id="22" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Privacy Policy</a></li>
+                        <li><a href="/site-map" id="23" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Sitemap</a></li>
+                        <li><a href="/plt-app-info" id="311" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">App Info</a></li>
+                    </ul>
+
+                </div>
             </section>
 
             <section className='flex flex-col items-center'>
-            <h2 class="mb-2.5 font-rebrand-light text-sm uppercase text-rebrand-accent-100-contrast">
-                My Account
-            </h2>
-            <ul class="flex flex-col items-center list-none gap-3 p-0 w-full">
-                <li><a href="/customer/order-history" rel="nofollow" id="25" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Order History</a></li>
-                <li><a href="/faq" id="26" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Track My Order</a></li>
-                <li><a href="/refer-a-friend" id="28" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Refer A Friend</a></li>
-                <li><a href="/cookies" id="382" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Cookies</a></li>
-            </ul>
+                <div className='flex flex-col items-start'>
+                    <h2 class="mb-2.5 font-rebrand-light text-sm uppercase text-rebrand-accent-100-contrast">
+                        My Account
+                    </h2>
+                    <ul class="flex flex-col list-none gap-3 p-0 w-full">
+                        <li><a href="/customer/order-history" rel="nofollow" id="25" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Order History</a></li>
+                        <li><a href="/faq" id="26" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Track My Order</a></li>
+                        <li><a href="/refer-a-friend" id="28" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Refer A Friend</a></li>
+                        <li><a href="/cookies" id="382" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Cookies</a></li>
+                    </ul>
+
+                </div>
             </section>
 
             <section className='flex flex-col items-center'>
-            <h2 class="mb-2.5 font-rebrand-light text-sm uppercase text-rebrand-accent-100-contrast">
-                Social
-            </h2>
-            <ul class="flex flex-col items-center list-none gap-3 p-0 w-full">
-                <li><a href="https://instagram.com/prettylittlething" id="312" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Instagram</a></li>
-                <li><a href="https://facebook.com/OfficialPLT" id="313" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Facebook</a></li>
-                <li><a href="https://twitter.com/OfficialPLT" id="314" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">X</a></li>
-                <li><a href="https://tiktok.com/share/user/6590702127344386054" id="315" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">TikTok</a></li>
-                <li><a href="https://youtube.com/user/prettylittlethingtv" id="316" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">YouTube</a></li>
-            </ul>
+                <div className='flex flex-col items-start'>
+
+                    <h2 class="mb-2.5 font-rebrand-light text-sm uppercase text-rebrand-accent-100-contrast">
+                        Social
+                    </h2>
+                    <ul class="flex flex-col list-none gap-3 p-0 w-full">
+                        <li><a href="https://instagram.com/prettylittlething" id="312" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Instagram</a></li>
+                        <li><a href="https://facebook.com/OfficialPLT" id="313" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">Facebook</a></li>
+                        <li><a href="https://twitter.com/OfficialPLT" id="314" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">X</a></li>
+                        <li><a href="https://tiktok.com/share/user/6590702127344386054" id="315" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">TikTok</a></li>
+                        <li><a href="https://youtube.com/user/prettylittlethingtv" id="316" class="font-rebrand-light text-xs capitalize text-rebrand-base-400 hover:underline">YouTube</a></li>
+                    </ul>
+                </div>
             </section>
 
         </nav>
