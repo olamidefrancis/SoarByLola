3fdc414479c9015f51172656f236cf29428742d6
deji2612 <olatigbay@gmail.com>
2025-11-28 17:47:39 +0000

    Checkout button fix

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -14,7 +14,7 @@ function Checkout() {
       <div className="w-full flex flex-col md:flex-row gap-3 paddycheck bg-[#f2e6c8] min-h-screen md:justify-center flex-[1,1]">
 
         {/* LEFT SIDE - ORDER SUMMARY */}
-        <div className="bg-white rounded-xl shadow-xl p-8  h-fit paddycheck flex flex-col gap-8 flex-1">
+        <div className=" relative bg-white rounded-xl shadow-xl p-8  h-fit paddycheck flex flex-col gap-8 flex-1">
 
           <h3 className="text-xl font-semibold mb-6 text-[#141414]">
             Order Summary
@@ -67,78 +67,79 @@ function Checkout() {
         </div>
 
         {/* RIGHT SIDE - FORM */}
-        <div className=" relative flex flex-col gap-6 rounded-xl shadow-xl paddycheck flex-1 bg-white">
+        <div className=" relative flex flex-col gap-2 rounded-xl shadow-xl paddycheck flex-1 bg-white">
+          <div className="flex flex-col gap-10 ">
 
-          <h2 className="text-3xl font-semibold tracking-wide mb-8 text-[#141414]">
-            Checkout
-          </h2>
+              <h2 className="text-3xl font-semibold tracking-wide mb-8 text-[#141414]">
+                Checkout
+              </h2>
 
-          {/* BILLING DETAILS */}
-          <div className="flex flex-col gap-6">
-            <h3 className="text-xl font-semibold text-[#141414]">Billing Details</h3>
+              {/* BILLING DETAILS */}
+              <div className="flex flex-col gap-6">
+                <h3 className="text-xl font-semibold text-[#141414]">Billing Details</h3>
 
-            <div className="flex flex-col md:flex-row gap-16">
-              <input type="text" placeholder="First Name"
-                className="checkout-input" />
-              <input type="text" placeholder="Last Name"
-                className="checkout-input" />
-            </div>
+                <div className="flex flex-col md:flex-row  gap-8  text-xl">
+                  <input type="text" placeholder="First Name"
+                    className="checkout-input flex-1" />
+                  <input type="text" placeholder="Last Name"
+                    className="checkout-input flex-1" />
+                </div>
 
-            <input type="email" placeholder="Email Address"
-              className="checkout-input w-full" />
-            <input type="tel" placeholder="Phone Number"
-              className="checkout-input w-full" />
-          </div>
-
-          {/* DELIVERY */}
-          <div className="flex flex-col gap-6">
-            <h3 className="text-xl font-semibold text-[#141414]">Delivery Address</h3>
-
-            <input type="text" placeholder="Street Address"
-              className="checkout-input w-full" />
-
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <input type="text" placeholder="City"
-                className="checkout-input" />
-              <input type="text" placeholder="Postcode"
-                className="checkout-input" />
-            </div>
-
-            <input type="text" placeholder="Country"
-              className="checkout-input w-full" />
-          </div>
-
-          {/* PAYMENT */}
-          <div className="flex flex-col gap-6">
-            <h3 className="text-xl font-semibold text-[#141414]">Payment</h3>
-
-            <div className="grid grid-cols-1 gap-6">
-              <input
-                type="text"
-                placeholder="Card Number"
-                className="checkout-input w-full"
-              />
-
-              <div className="grid grid-cols-2 gap-6">
-                <input
-                  type="text"
-                  placeholder="MM/YY"
-                  className="checkout-input"
-                />
-                <input
-                  type="text"
-                  placeholder="CVV"
-                  className="checkout-input"
-                />
+                <input type="email" placeholder="Email Address"
+                  className="checkout-input w-full text-xl" />
+                <input type="tel" placeholder="Phone Number"
+                  className="checkout-input w-full text-xl" />
               </div>
-            </div>
+
+              {/* DELIVERY */}
+              <div className="flex flex-col gap-6 text-xl">
+                <h3 className="text-xl font-semibold text-[#141414]">Delivery Address</h3>
+
+                <input type="text" placeholder="Street Address"
+                  className="checkout-input w-full" />
+
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <input type="text" placeholder="City"
+                    className="checkout-input" />
+                  <input type="text" placeholder="Postcode"
+                    className="checkout-input" />
+                </div>
+
+                <input type="text" placeholder="Country"
+                  className="checkout-input w-full" />
+              </div>
+
+              {/* PAYMENT */}
+              <div className="flex flex-col gap-6 text-xl">
+                <h3 className="text-xl font-semibold text-[#141414]">Payment</h3>
+
+                <div className="grid grid-cols-1 gap-6">
+                  <input
+                    type="text"
+                    placeholder="Card Number"
+                    className="checkout-input w-full"
+                  />
+
+                  <div className="grid grid-cols-2 gap-6">
+                    <input
+                      type="text"
+                      placeholder="MM/YY"
+                      className="checkout-input"
+                    />
+                    <input
+                      type="text"
+                      placeholder="CVV"
+                      className="checkout-input"
+                    />
+                  </div>
+                </div>
+              </div>
           </div>
 
-          {/* PLACE ORDER */}
-          <button className="absolute mt-10 w-full max-w-sm bg-[#141414] text-[#f2e6c8] py-4 rounded-xl tracking-widest text-lg uppercase hover:opacity-90 transition paddycheck md:bottom-10 left-1/2 -translate-x-1/2 bottom-0">
-
-            Place Order
+          <button class="absolute mt-10 w-full max-w-sm bg-[#141414] text-[#f2e6c8] py-4 rounded-xl tracking-widest text-lg uppercase hover:opacity-90 transition paddycheck md:bottom-10 left-1/2 -translate-x-1/2 bottom-0">
+          Place Order
           </button>
+         
         </div>
 
         
